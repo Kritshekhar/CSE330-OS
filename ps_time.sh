27cba0f9dfabca87446bafac298a530a20e51935
Kritshekhar <kjha9@asu.edu>
2022-02-12 11:28:32 -0700

    ps script

---
+++ ps_time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+let TIME=0
+echo $1
+for pid in $(ps -u $1 | awk '{print $1}'); do
+	if [[ "$pid" == "PID" ]]; then
+		continue
+	else
+		TMP=$(ps -p $pid -o etimes=)
+		let TIME=$((TIME+TMP))
+	fi
+done
+HR=$(($TIME/3600))
+MIN=$((($TIME-3600*$HR)/60))
+SEC=$((($TIME-3600*$HR)-($MIN*60)))
+
+#echo "Total Time Elapsed $HR:$MIN:$SEC"
+op="$HR:$MIN:$SEC"
+echo "Total Elapsed Time from PS Command $op"
