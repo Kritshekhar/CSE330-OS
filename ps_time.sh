b549b02f8792fabe7f48ec247f88093882b66f5d
Kritshekhar <kjha9@asu.edu>
2022-03-08 09:49:07 -0700

    Test script updated

--- ps_time.sh
+++ ps_time.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 let TIME=0
-for pid in $(ps -u $1 | awk '{print $1}'); do
-	if [[ "$pid" == "PID" ]]; then
-		continue
-	else
-		TMP=$(ps -p $pid -o etimes=)
-		let TIME=$((TIME+TMP))
-	fi
+etimes=$(ps -u $1 -o etimes= | awk '{print $1}')
+etimesList=($etimes)
+etimesLen=${#etimesList[@]}
+for ((j=0; j<${etimesLen}; j++)); do
+	let TIME=$((TIME+${etimesList[$j]}))
 done
+
 HR=$(($TIME/3600))
 MIN=$((($TIME-3600*$HR)/60))
 SEC=$((($TIME-3600*$HR)-($MIN*60)))
 
-#echo "Total Time Elapsed $HR:$MIN:$SEC"
 op="$HR:$MIN:$SEC"
 echo "Total Elapsed Time from PS Command $op"
--- test.sh
+++ test.sh
@@ -11,12 +11,14 @@ make
 echo "Test script will start $num processes for the user $uid"
 sudo su $name -c ./process_gen/process_generator &
 sleep 10
-PS_SCRIPT=`bash ps_time.sh $uid`
+PS_SCRIPT=`bash ps_time.sh $uid &`
 let OUTPUT
 OUTPUT=$PS_SCRIPT; 
-echo "Test script will insert the test kernel module"
+echo "The test script will now insert the test kernel module."
 sudo insmod producer_consumer.ko uid=$uid buff_size=$2 p=$3 c=$4 && 
-echo "Test script will remove the test kernel module"
+echo "The test script waits 10 seconds for the kernel module to finish."
+sleep 10
+echo "The test script will now remove the kernel module."
 sudo rmmod producer_consumer; 
 echo "The following is the dmesg output"
 dmesg | tail -n $5; echo $OUTPUT; 
